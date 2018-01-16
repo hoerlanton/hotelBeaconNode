93604296f82dc29497549e9a1d39bec38add8fa0
Anton Hörl <anton.hoerl@gmx.at>
2018-01-16 10:29:18 +0100

    Test3

--- app.js
+++ app.js
@@ -13,17 +13,15 @@ var request = require('request');
 var http = require('http');
 var fs = require("fs");
 
-
-
-
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+
+app.use(bodyParser.json());
 
 //Setting port
 app.set('port', process.env.PORT || 8000);
